adcde2965bc352cf1971f7eba098f15a86405643
Prod4 <prod4@amerisol.net>
2015-04-23 14:12:29 -0600

    Some Css Fixes

--- js/logic.js
+++ js/logic.js
@@ -1,12 +1,12 @@
 (function($){
     //We define the plugin.
-    $.fn.starsCf = function(options) {
+    $.fn.cloudsCf = function(options) {
         var $elm = this;
 
         //We define the defaults.
         var config = $.extend({
             'cantidad_nubes' : 1,
-            'tamanno' : 'big', //Options big - small - medium
+            'tamanno' : [25, 50],
             'color' : '#fff', //Options default - other
             'speed' : 'normal' //Speed of the animation
         }, options);
@@ -18,16 +18,16 @@
        $elm.append('<div class="clouds-container"></div>');
 
        for(i=0; i < options.cantidad_nubes; i++){
-        $('.clouds-container').append('<div class="cloud"></div>')
+          $('.clouds-container').append('<div class="cloud"></div>')
        }
 
        var clouds = $('.cloud');
-       $.each(clouds, function(i,v){
-        //???
-       })
+
+       // clouds.each(function () {
+       //    // setCloudSize($(this), options);
+       // });
 
        $('.cloud').css('background', options.color);
        $('body').append('<style>.cloud:after{background: ' + options.color + ';}.cloud:before{background: ' + options.color + ';}</style>');
     }
 }(jQuery))
-
